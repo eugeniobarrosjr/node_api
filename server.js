7aac6975b2cdefac891d42eea34e1cbc673b7828
Eugênio Barros <eugenio.barros.jr@gmail.com>
2020-08-14 14:17:06 -0300

    feature: finish app

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+requireDir('./src/models');
+
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
